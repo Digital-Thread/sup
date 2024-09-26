fb5b32556aac7b9c0b7725bc395d9ee4002dc9a0
Elena Vedeneva <astroshelest@gmail.com>
2024-09-26 12:12:33 +0200

    Added exceptions for service layer operations

---
+++ src/apps/feature/exceptions.py
@@ -0,0 +1,33 @@
+from uuid import UUID
+
+
+class FeatureError(Exception):
+    pass
+
+
+class FeatureDoesNotExistError(FeatureError):
+    def __init__(self, feature_id: UUID, message: str = None):
+        self.feature_id = feature_id
+        self.message = message or f'Фича с ID {self.feature_id} не найдена.'
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class FeatureCreateError(FeatureError):
+    def __init__(self, message: str = 'Ошибка при создании фичи'):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class FeatureUpdateError(FeatureError):
+    def __init__(self, message: str = 'Ошибка при обновлении фичи'):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return self.message
