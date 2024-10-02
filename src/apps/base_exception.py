6469227ef61b1759e197d8c2042ff4335c90af4a
Daniel Makhanek <daniel.makhanek@gmail.com>
2024-10-02 17:18:50 +0300

    fix base exceptions

--- src/apps/base_exception.py
+++ src/apps/base_exception.py
@@ -1,25 +1,4 @@
-# from dataclasses import dataclass
-
-
-# @dataclass(eq=False)
-# class ApplicationError(Exception):
-#     """Base error"""
-
-#     status_code: int = 500
-#     message: str = 'Internal Server Error'
-
-#     def __post_init__(self):
-#         super().__init__(self.message)
-
-
 class ApplicationError(Exception):
-    """Base error"""
+    """Base exception for application errors"""
 
-    def __init__(
-        self,
-        status_code: int = 500,
-        message: str = 'Internal Server Error',
-    ):
-        self.status_code = status_code
-        self.message = message
-        super().__init__(self.message)
+    pass
