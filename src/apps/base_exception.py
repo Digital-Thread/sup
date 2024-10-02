64da584534f23cce0b0202e875b30de05b4da0ef
Daniel Makhanek <daniel.makhanek@gmail.com>
2024-10-02 17:18:50 +0300

    fix base exceptions

--- src/apps/base_exception.py
+++ src/apps/base_exception.py
@@ -1,13 +1,4 @@
-import dataclasses
-from typing import ClassVar
-
-
-@dataclasses.dataclass(eq=False)
 class ApplicationError(Exception):
-    """Base Error"""
+    """Base exception for application errors"""
 
-    status_code: ClassVar[int] = 500
-
-    @property
-    def message(self) -> str:
-        return 'An app error occurred'
+    pass
