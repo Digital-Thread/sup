af0d0f297c20ce8fcbc37cbce6e1da95622e1b17
Daniel Makhanek <daniel.makhanek@gmail.com>
2024-10-05 18:48:13 +0300

    add UUID mixin

--- src/data_access/models/mixins.py
+++ src/data_access/models/mixins.py
@@ -1,14 +1,23 @@
+import uuid
 from datetime import datetime
 from typing import Annotated
 
 from sqlalchemy import TIMESTAMP, func
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
 pk = Annotated[int, mapped_column(primary_key=True, autoincrement=True, unique=True)]
+pk_uuid = Annotated[
+    uuid.UUID, mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True)
+]
 
 
 class IntIdPkMixin:
     id: Mapped[pk]
+
+
+class UUIDPkMixin:
+    id: Mapped[pk_uuid]
 
 
 class DatetimeFieldsMixin:
--- src/data_access/models/user.py
+++ src/data_access/models/user.py
@@ -1,10 +1,10 @@
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
-from .mixins import DatetimeFieldsMixin, IntIdPkMixin
+from .mixins import DatetimeFieldsMixin, UUIDPkMixin
 
 
-class User(Base, DatetimeFieldsMixin, IntIdPkMixin):
+class User(Base, DatetimeFieldsMixin, UUIDPkMixin):
     first_name: Mapped[str]
     last_name: Mapped[str] = mapped_column(unique=True, nullable=True)
     email: Mapped[str] = mapped_column(unique=True, nullable=True)
