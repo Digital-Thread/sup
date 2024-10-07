f594d1821a7f9d283192d26805e154063ad9735e
Sergey Komarov <onubee@gmail.com>
2024-10-07 20:02:19 +0300

    creat model user

--- src/data_access/models/user.py
+++ src/data_access/models/user.py
@@ -1,10 +1,20 @@
+from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
-from .mixins import DatetimeFieldsMixin, IntIdPkMixin
+from .mixins import DatetimeFieldsMixin, UUIDPkMixin
 
 
-class User(Base, DatetimeFieldsMixin, IntIdPkMixin):
-    first_name: Mapped[str]
-    last_name: Mapped[str] = mapped_column(unique=True, nullable=True)
-    email: Mapped[str] = mapped_column(unique=True, nullable=True)
+class User(Base, DatetimeFieldsMixin, UUIDPkMixin):
+    first_name: Mapped[str] = mapped_column(String(20), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(20), nullable=False)
+    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(50), nullable=False)
+    username_tg: Mapped[str] = mapped_column(String(50), nullable=False)
+    nick_tg: Mapped[str] = mapped_column(String(50), nullable=False)
+    nick_gmeet: Mapped[str] = mapped_column(String(50), nullable=False)
+    nick_gitlab: Mapped[str] = mapped_column(String(50), nullable=True)
+    nick_github: Mapped[str] = mapped_column(String(50), nullable=True)
+    avatar: Mapped[str] = mapped_column(String, nullable=True)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False)
