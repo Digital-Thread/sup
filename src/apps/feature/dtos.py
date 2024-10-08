42a6c9858eafd828ea879aa475174a350ef4b6d4
Elena Vedeneva <astroshelest@gmail.com>
2024-10-08 18:03:12 +0200

    Created a FeatureOutputDTO

--- src/apps/feature/dtos.py
+++ src/apps/feature/dtos.py
@@ -1,8 +1,14 @@
-from dataclasses import dataclass, field
-from typing import TypedDict
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Self
 
 from apps.feature.domain.aliases import FeatureId, OwnerId, ProjectId, TagId, UserId
-from apps.feature.domain.entities.feature import Priority, Status
+from apps.feature.domain.entities.feature import (
+    Feature,
+    OptionalFeatureUpdateFields,
+    Priority,
+    Status,
+)
 
 
 @dataclass
@@ -18,18 +24,40 @@ class FeatureInputDTO:
     members: set[UserId] | None = None
 
 
-class OptionalFeatureUpdateFields(TypedDict, total=False):
-    name: str
-    project_id: ProjectId
-    assigned_to: UserId | None
-    description: str | None
-    priority: Priority
-    status: Status
-    tags: set[TagId] | None
-    members: set[UserId] | None
-
-
 @dataclass
 class FeatureUpdateDTO:
     id: FeatureId
     updated_fields: OptionalFeatureUpdateFields
+
+
+@dataclass
+class FeatureOutputDTO:
+    id: FeatureId
+    name: str
+    project_id: ProjectId
+    owner_id: OwnerId
+    created_at: datetime
+    updated_at: datetime
+    assigned_to: UserId | None = None
+    description: str | None = None
+    priority: Priority = Priority.NO_PRIORITY
+    status: Status = Status.NEW
+    tags: set[TagId] | None = None
+    members: set[UserId] | None = None
+
+    @classmethod
+    def from_entity(cls, feature_id: FeatureId, entity: Feature) -> Self:
+        return cls(
+            id=feature_id,
+            name=entity.name,
+            project_id=entity.project_id,
+            owner_id=entity.owner_id,
+            created_at=entity.created_at,
+            updated_at=entity.updated_at,
+            assigned_to=entity.assigned_to,
+            description=entity.description,
+            priority=entity.priority,
+            status=entity.status,
+            tags=set(entity.tags) if entity.tags else None,
+            members=set(entity.members) if entity.members else None,
+        )
