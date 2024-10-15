6399d5f5fb4dfa6d5f0d2d2994acb05df1cb6540
dromanov <dtdzhalaev@gmail.com>
2024-10-15 20:28:03 +0300

    ♻️ minor fixes

--- src/apps/comment/interactors/add_comment_to_task.py
+++ src/apps/comment/interactors/add_comment_to_task.py
@@ -1,12 +1,10 @@
 from src.apps.comment import AddCommentToTaskDto, CommentOutDto
+from src.apps.comment.converters import CommentMapper
 from src.apps.comment.domain import (
-    AuthorId,
     CommentEntity,
     CommentId,
-    Content,
     ICommentRepository,
     Interactor,
-    TaskId,
 )
 
 
@@ -16,13 +14,8 @@ class AddCommentToTaskInteractor(Interactor[AddCommentToTaskDto, CommentOutDto])
         self._repository = comment_repository
 
     def execute(self, request: AddCommentToTaskDto) -> CommentOutDto:
-        comment_entity = CommentEntity.create_for_entity(
-            author_id=AuthorId(request.author_id),
-            feature_id=request.feature_id,
-            task_id=TaskId(request.task_id),
-            content=Content(request.content),
-        )
+        comment_entity = CommentMapper.dto_to_entity(request)
 
         c: CommentEntity = self._repository.save(comment_entity)
-        response_dto: CommentOutDto = c.to_dto()
+        response_dto: CommentOutDto = CommentMapper.entity_to_dto(c)
         return response_dto
