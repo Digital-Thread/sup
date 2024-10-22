006ecf6b891a0e89699d493ecfa6f5bc8df2e53c
MaestroXXXVIII <urmanov.pro@gmail.com>
2024-10-22 15:55:44 +0800

    deleted the service

--- src/apps/workspace/services/category_service.py
+++
@@ -1,8 +0,0 @@
-from src.apps.workspace.domain.entities.category import Category
-from src.apps.workspace.dtos.category_dtos import CategoryAppDTO
-from src.apps.workspace.repositories.i_category_repository import ICategoryRepository
-from src.apps.workspace.services.base_service import BaseService
-
-
-class CategoryService(BaseService[Category, CategoryAppDTO, int, ICategoryRepository]):
-    pass
