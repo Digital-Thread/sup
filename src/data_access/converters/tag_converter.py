2d0ad80b25b4e8dd8a628e5ae6c98039f174e08d
MaestroXXXVIII <urmanov.pro@gmail.com>
2024-10-23 22:35:16 +0800

    fix converter and add method entity_to_dict

--- src/data_access/converters/tag_converter.py
+++ src/data_access/converters/tag_converter.py
@@ -1,22 +1,28 @@
 from src.apps.workspace.domain.entities.tag import Tag
+from src.apps.workspace.domain.types_ids import WorkspaceId
 from src.data_access.models.workspace_models.tag import TagModel
 
 
-class TagConverter[T]:
+class TagConverter:
     @staticmethod
-    def model_to_entity(tag_model: T) -> Tag:
-        clean_data = {
-            column.name: getattr(tag_model, column.name) for column in tag_model.__table__.columns
-        }
-        role = Tag(
-            _id=clean_data['id'],
-            _name=clean_data['name'],
-            _color=clean_data['color'],
-            _workspace_id=clean_data['workspace_id'],
+    def model_to_entity(tag_model: TagModel) -> Tag:
+        return Tag(
+            _id=tag_model.id,
+            _name=tag_model.name,
+            _color=tag_model.color,
+            _workspace_id=WorkspaceId(tag_model.workspace_id),
         )
-        return role
 
     @staticmethod
     def entity_to_model(entity: Tag) -> TagModel:
         model = TagModel(**entity.__dict__)
         return model
+
+    @staticmethod
+    def entity_to_dict(tag: Tag) -> dict:
+        return {
+            'id': tag.id,
+            'name': tag.name,
+            'color': tag.color,
+            'workspace_id': tag.workspace_id,
+        }
