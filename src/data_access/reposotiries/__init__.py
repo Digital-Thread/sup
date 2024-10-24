990018d6ca9a85a269b824173aa69c735b33c1a4
MaestroXXXVIII <urmanov.pro@gmail.com>
2024-10-24 15:49:06 +0800

    added di for repositories

---
+++ src/data_access/reposotiries/__init__.py
@@ -0,0 +1,13 @@
+from .category_respository import CategoryRepository
+from .role_repository import RoleRepository
+from .tag_repository import TagRepository
+from .workspace_invite_repository import WorkspaceInviteRepository
+from .workspace_repository import WorkspaceRepository
+
+__all__ = (
+    'CategoryRepository',
+    'RoleRepository',
+    'TagRepository',
+    'WorkspaceRepository',
+    'WorkspaceInviteRepository',
+)
