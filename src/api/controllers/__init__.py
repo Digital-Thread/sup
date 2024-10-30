b94ff69a54866607b8eb974c63a59319ba550686
MaestroXXXVIII <urmanov.pro@gmail.com>
2024-10-30 11:24:56 +0800

    added api for category

--- src/api/controllers/__init__.py
+++ src/api/controllers/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from .category import category_router
 from .health_check import router
 from .role import role_router
 from .tag import tag_router
@@ -16,6 +17,7 @@ def init_routes(app: FastAPI) -> None:
     app.include_router(router=workspace_router, prefix=f'{prefix}/workspace', tags=['Workspace'])
     app.include_router(router=role_router, prefix=f'{prefix}/role', tags=['Role'])
     app.include_router(router=tag_router, prefix=f'{prefix}/tag', tags=['Tag'])
+    app.include_router(router=category_router, prefix=f'{prefix}/category', tags=['Category'])
 
 
 __all__ = ('init_routes',)
