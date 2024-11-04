d34f3b2adc7c0c2ac5da70358203b5203a7629c2
Elena Vedeneva <astroshelest@gmail.com>
2024-11-04 17:55:25 +0100

    added feature_router

--- src/api/controllers/__init__.py
+++ src/api/controllers/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from .feature import feature_router
 from .health_check import router
 
 
@@ -10,6 +11,7 @@ def init_routes(app: FastAPI) -> None:
         prefix=f'{prefix}/health-check',
         tags=['Test'],
     )
+    app.include_router(router=feature_router, prefix=f'{prefix}/features', tags=['Feature'])
 
 
 __all__ = ('init_routes',)
