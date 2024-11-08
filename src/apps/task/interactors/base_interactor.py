96167c79234afbcade36f212692b84f4cca63108
Elena Vedeneva <astroshelest@gmail.com>
2024-11-08 21:04:55 +0100

    Implemented task interactors logic

---
+++ src/apps/task/interactors/base_interactor.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+from src.apps.task.repositories import ITaskRepository
+
+
+class BaseInteractor(ABC):
+    def __init__(self, task_repository: ITaskRepository):
+        self._repository = task_repository
+
+    @abstractmethod
+    async def execute(self, *args: Any) -> Any: ...
