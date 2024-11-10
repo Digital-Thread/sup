6aa3badbf884afaf2d1f4da6d7c2f944cbf6dece
Daniel Makhanek <daniel.makhanek@gmail.com>
2024-11-10 11:20:47 +0300

    fix meet alembic

--- src/data_access/models/meet.py
+++ src/data_access/models/meet.py
@@ -19,13 +19,13 @@ class MeetModel(Base, IntIdPkMixin, DatetimeFieldsMixin):
 
     name: Mapped[str]
     meet_at: Mapped[datetime]
-    workspace_id: Mapped[UUID] = mapped_column(ForeignKey('workspace.id'))
-    category_id: Mapped[int] = mapped_column(ForeignKey('category.id'))
-    owner_id: Mapped[UUID] = mapped_column(ForeignKey('user.id'))
-    assigned_to: Mapped[UUID] = mapped_column(ForeignKey('user.id'))
+    workspace_id: Mapped[UUID] = mapped_column(ForeignKey('workspaces.id'))
+    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'))
+    owner_id: Mapped[UUID] = mapped_column(ForeignKey('users.id'))
+    assigned_to: Mapped[UUID] = mapped_column(ForeignKey('users.id'))
 
     participants: Mapped[list['ParticipantModel']] = relationship(
-        cascade='all, delete-orphan', back_populates='meet', lazy='raise_on_sql'
+        cascade='all, delete-orphan', back_populates='meets', lazy='raise_on_sql'
     )
 
     owner: Mapped['UserModel'] = relationship(
@@ -40,7 +40,6 @@ class MeetModel(Base, IntIdPkMixin, DatetimeFieldsMixin):
         'Workspace', foreign_keys=[workspace_id], back_populates='meets', lazy='raise_on_sql'
     )
 
-    # TODO: Decide about cascade delete or not
     category: Mapped['CategoryModel'] = relationship(
         'Category', back_populates='meets', lazy='joined'
     )
