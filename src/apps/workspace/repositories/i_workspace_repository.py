55885d755cfbc0d1325650b781cb0af53043c302
MaestroXXXVIII <urmanov.pro@gmail.com>
2024-11-12 15:11:02 +0800

    created repo for user workspace role

--- src/apps/workspace/repositories/i_workspace_repository.py
+++ src/apps/workspace/repositories/i_workspace_repository.py
@@ -1,8 +1,7 @@
 from abc import abstractmethod
 
 from src.apps.workspace.domain.entities.workspace import Workspace
-from src.apps.workspace.domain.types_ids import MemberId, OwnerId, RoleId, WorkspaceId
-from src.apps.workspace.repositories.base_repository import IBaseRepository
+from src.apps.workspace.domain.types_ids import MemberId, OwnerId, WorkspaceId
 
 
 class IWorkspaceRepository:
@@ -24,12 +23,6 @@ class IWorkspaceRepository:
 
     @abstractmethod
     async def find_by_member_id(self, member_id: MemberId) -> list[Workspace]:
-        raise NotImplementedError
-
-    @abstractmethod
-    async def assign_role_to_user(
-        self, workspace_id: WorkspaceId, user_id: OwnerId, role_id: RoleId
-    ) -> None:
         raise NotImplementedError
 
     @abstractmethod
--- src/data_access/repositories/workspace_repository.py
+++ src/data_access/repositories/workspace_repository.py
@@ -4,12 +4,9 @@ from asyncpg.exceptions import ForeignKeyViolationError
 from sqlalchemy import delete, exists, select, update
 from sqlalchemy.exc import IntegrityError, NoResultFound
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload
 
-from src.apps.workspace.domain.entities.role import Role
 from src.apps.workspace.domain.entities.workspace import Workspace
-from src.apps.workspace.domain.types_ids import MemberId, OwnerId, RoleId, WorkspaceId
-from src.apps.workspace.exceptions.role_exceptions import RoleNotFound
+from src.apps.workspace.domain.types_ids import MemberId, OwnerId, WorkspaceId
 from src.apps.workspace.exceptions.workspace_exceptions import (
     MemberWorkspaceNotFound,
     WorkspaceAlreadyExists,
@@ -17,12 +14,8 @@ from src.apps.workspace.exceptions.workspace_exceptions import (
     WorkspaceNotUpdated,
 )
 from src.apps.workspace.repositories.i_workspace_repository import IWorkspaceRepository
-from src.data_access.converters.role_converter import RoleConverter
 from src.data_access.converters.workspace_converter import WorkspaceConverter
 from src.data_access.models import (
-    RoleModel,
-    UserModel,
-    UserWorkspaceRoleModel,
     WorkspaceMemberModel,
     WorkspaceModel,
 )
@@ -109,62 +102,3 @@ class WorkspaceRepository(IWorkspaceRepository):
             await self._session.flush()
         except IntegrityError as error:
             warning(error)
-
-    async def assign_role_to_user(
-        self, workspace_id: WorkspaceId, user_id: OwnerId, role_id: RoleId
-    ) -> None:
-        user_exists = await self._session.execute(select(exists().where(UserModel.id == user_id)))
-
-        if not user_exists.scalar():
-            pass
-            # TODO выбросить not found user
-
-        workspace_exists = await self._session.execute(
-            select(exists().where(WorkspaceModel.id == workspace_id))
-        )
-
-        if not workspace_exists.scalar():
-            raise WorkspaceNotFound('Рабочее пространство не найдено')
-
-        role_exists = await self._session.execute(select(exists().where(RoleModel.id == role_id)))
-
-        if not role_exists.scalar():
-            raise RoleNotFound('Роль не найдена')
-
-        user_role_exists = await self._session.execute(
-            select(
-                exists().where(
-                    UserWorkspaceRoleModel.user_id == user_id,
-                    UserWorkspaceRoleModel.workspace_id == workspace_id,
-                )
-            )
-        )
-
-        if user_role_exists.scalar():
-            await self._session.execute(
-                update(UserWorkspaceRoleModel)
-                .filter_by(user_id=user_id, workspace_id=user_id)
-                .values(role_id=role_id)
-            )
-        else:
-            stmt = UserWorkspaceRoleModel(
-                user_id=user_id, workspace_id=workspace_id, role_id=role_id
-            )
-            self._session.add(stmt)
-
-        await self._session.commit()
-
-    async def find_user_role_in_workspace(
-        self, user_id: OwnerId, workspace_id: WorkspaceId
-    ) -> Role | None:
-        query = (
-            select(UserWorkspaceRoleModel)
-            .options(joinedload(UserWorkspaceRoleModel.role))
-            .filter_by(user_id=user_id, workspace_id=workspace_id)
-        )
-        result = await self._session.execute(query)
-        user_workspace_role = result.scalars().first()
-        role = (
-            RoleConverter.model_to_entity(user_workspace_role.role) if user_workspace_role else None
-        )
-        return role
