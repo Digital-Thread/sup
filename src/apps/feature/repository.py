d04b7c0e161aa5bc0a7bcc774ebfc3151ee53f01
Elena Vedeneva <astroshelest@gmail.com>
2024-11-25 18:09:34 +0100

    Separate query_parameters and repository file

--- src/apps/feature/repository.py
+++ src/apps/feature/repository.py
@@ -1,55 +1,11 @@
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from enum import Enum
-from typing import Literal, NamedTuple, TypedDict
 
+from src.apps.feature.query_parameters import FeatureListQuery
 from src.apps.feature.domain import (
     FeatureEntity,
     FeatureId,
-    ProjectId,
-    Status,
-    TagId,
-    UserId,
     WorkspaceId,
 )
-
-
-class OrderByField(Enum):
-    NAME = 'name'
-    ASSIGNED_TO = 'assigned_to_id'
-    CREATED_AT = 'created_at'
-    PRIORITY = 'priority'
-    PROJECT = 'project_id'
-    STATUS = 'status'
-
-
-class SortOrder(Enum):
-    ASC = 'ASC'
-    DESC = 'DESC'
-
-
-class OrderBy(NamedTuple):
-    field: OrderByField
-    order: SortOrder
-
-
-class FilterField(TypedDict, total=False):
-    members: list[UserId]
-    tags: list[TagId]
-    status: list[Status]
-    project: list[ProjectId]
-
-
-class PaginateParams(NamedTuple):
-    offset: int
-    limit_by: Literal[5, 10, None]
-
-
-@dataclass
-class FeatureListQuery:
-    filters: FilterField | None = None
-    order_by: OrderBy | None = OrderBy(OrderByField.PRIORITY, SortOrder.DESC)
-    paginate_by: PaginateParams = PaginateParams(offset=0, limit_by=10)
 
 
 class IFeatureRepository(ABC):
@@ -72,6 +28,6 @@ class IFeatureRepository(ABC):
 
     @abstractmethod
     async def get_list(
-        self, workspace_id: WorkspaceId, query: FeatureListQuery
+            self, workspace_id: WorkspaceId, query: FeatureListQuery
     ) -> list[tuple[FeatureId, FeatureEntity]]:
         pass
