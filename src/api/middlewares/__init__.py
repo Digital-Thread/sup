1ef48cbefa0bdf31456aa7a19453b298a5232d9a
Urmanov Oleg <86904752+MaestroXXXVIII@users.noreply.github.com>
2024-12-03 06:54:50 +0800

    feat(middleware): add middleware with provider to handle workspace_id in all requests (#40)
    * add middleware with provider to handle workspace_id in all requests
    
    * minor fix(temporary)
    
    * add header(x-workspace_id) in openapi schema
    
    * add handle excluded routes by tags

--- src/api/middlewares/__init__.py
+++ src/api/middlewares/__init__.py
@@ -4,6 +4,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 from .logging import logging_middleware
 from .update_tokens import update_tokens_middleware
+from .workspace import WorkspaceMiddleware
 
 
 def init_middlewares(app: FastAPI) -> None:
@@ -16,6 +17,7 @@ def init_middlewares(app: FastAPI) -> None:
         BaseHTTPMiddleware,
         dispatch=update_tokens_middleware,
     )
+    app.add_middleware(WorkspaceMiddleware)
     app.add_middleware(
         CORSMiddleware,
         allow_origins=['*'],
