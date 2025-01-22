5d4a0051d92d2f65064510c5b3d9ad6d6e022626
Daniel Makhanek <daniel.makhanek@gmail.com>
2025-01-22 12:48:50 +0100

    refactor meet exceptions

--- src/apps/meet/exceptions.py
+++ src/apps/meet/exceptions.py
@@ -1,47 +1,50 @@
+from typing import Any
+
 from src.apps import ApplicationException
 
 
-class BaseMeetException(ApplicationException):
-    def __init__(self, message: str = 'Meet error'):
-        self.message = message
-        super().__init__(self.message)
+class MeetError(ApplicationException):
+    DEFAULT_MESSAGE = 'Meet error'
+
+    def __init__(self, context: Exception | None = None, message: str | None = None) -> None:
+        self.context = context
+        self.message = message or self.DEFAULT_MESSAGE
+        self.args: tuple[Any, ...] = (self.context, self.message)
+
+    def __str__(self) -> str:
+        if self.context:
+            return f'{self.message} {self.context}'
+
+        return self.message
 
 
-class AccessDeniedException(BaseMeetException):
-    def __init__(self, message: str = 'Access denied'):
-        super().__init__(message)
+class MeetAccessDeniedError(MeetError):
+    DEFAULT_MESSAGE = 'Meet access denied'
 
 
-class MeetNameLengthException(BaseMeetException):
-    def __init__(self, message: str = 'Meet name length error'):
-        super().__init__(message)
+class MeetNameLengthError(MeetError):
+    DEFAULT_MESSAGE = 'Meet name length error'
 
 
-class MeetNotFoundException(BaseMeetException):
-    def __init__(self, message: str = 'Meet not found'):
-        super().__init__(message)
+class MeetNotFoundError(MeetError):
+    DEFAULT_MESSAGE = 'Meet not found'
 
 
-class MeetCreateException(BaseMeetException):
-    def __init__(self, message: str = 'Meet create error'):
-        super().__init__(message)
+class MeetCreateError(MeetError):
+    DEFAULT_MESSAGE = 'Meet create error'
 
 
-class MeetInviteException(BaseMeetException):
-    def __init__(self, message: str = 'Meet invite error'):
-        super().__init__(message)
+class MeetInviteError(MeetError):
+    DEFAULT_MESSAGE = 'Meet invite error'
 
 
-class ParticipantCheckException(BaseMeetException):
-    def __init__(self, message: str = 'Participant check error'):
-        super().__init__(message)
+class ParticipantCheckError(MeetError):
+    DEFAULT_MESSAGE = 'Participant check error'
 
 
-class ParticipantNotFoundException(BaseMeetException):
-    def __init__(self, message: str = 'Participant not found'):
-        super().__init__(message)
+class ParticipantNotFoundException(MeetError):
+    DEFAULT_MESSAGE = 'Participant not found'
 
 
-class ParticipantCreateException(BaseMeetException):
-    def __init__(self, message: str = 'Participant create error'):
-        super().__init__(message)
+class ParticipantCreateError(MeetError):
+    DEFAULT_MESSAGE = 'Participant create error'
