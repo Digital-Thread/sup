320fc15318defb5120fb789b717f6ff9147c0b95
Daniel Makhanek <daniel.makhanek@gmail.com>
2025-01-22 15:37:20 +0100

    refactor meet dtos

---
+++ src/apps/meet/query_parameters.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+from typing import Literal, NamedTuple, TypedDict
+
+from .domain.type_ids import (
+    AssignedId,
+    CategoryId,
+)
+
+
+class OrderByField(Enum):
+    NAME = 'name'
+    ASSIGNED_TO = 'assigned_to'
+    MEET_AT = 'meet_at'
+
+
+class SortOrder(Enum):
+    ASC = 'ASC'
+    DESC = 'DESC'
+
+
+class OrderBy(NamedTuple):
+    field: OrderByField
+    order: SortOrder
+
+
+class FilterField(TypedDict, total=False):
+    category: CategoryId
+    assigned_to: AssignedId
+    meet_at: datetime
+
+
+class PaginateParams(NamedTuple):
+    offset: int = 0
+    limit_by: Literal[4, 8, 16, 24] | None = 16
+
+
+@dataclass
+class MeetListQuery:
+    filters: FilterField | None = None
+    order_by: OrderBy = OrderBy(OrderByField.MEET_AT, SortOrder.DESC)
+    paginate_by: PaginateParams = PaginateParams(offset=0, limit_by=16)
+
+
+# class SortBy(NamedTuple):
+#     field: Literal['name', 'assigned_to', 'meet_at']
+#     order: Literal['ASC', 'DESC']
+
+
+# class MeetFilterFields(TypedDict, total=False):
+#     category: CategoryId
+#     assigned_to: AssignedId
+#     meet_at: datetime
+
+
+# @dataclass
+# class MeetListQuery:
+#     filters: MeetFilterFields | None = None
+#     order_by: SortBy = SortBy('meet_at', 'DESC')
+#     limit: Literal[4, 8, 16, 24] | None = 16
+#     offset: int = 0
